c41cafb0147822a71657892fa5dfb2fa05883109
hyojin.sarchet <hyojin5931@hanmail.net>
2017-08-31 00:29:16 -0400

    edited app.js

--- js/engine.js
+++ js/engine.js
@@ -97,6 +97,10 @@ var Engine = (function(global) {
         player.update();
     }
 
+
+    // allEnemies.forEach(function(enemy) {
+    //     enemy.update(player);
+    // });
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
      * game tick (or loop of the game engine) because that's how games work -
